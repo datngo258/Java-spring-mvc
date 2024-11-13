b35e8608c5f2309447ccd994c7d03ba598591b20
ngodinhdat258@gmail.com <ngodinhdat258@gmail.com>
2024-11-13 21:07:32 +0700

    video 33: devToool

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,7 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello World from Spring Boođâsdasdasdasdasdasdasdasdasdadasdat!";
+        return "Hello World from adsdas đá Spring !";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "only user";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "only admin!";
     }
 
 }
